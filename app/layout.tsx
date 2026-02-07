e53fc7d45b656c7c1384f3bece4f74526b96ee33
Guilherme Abatemarco <guiabatemarco@hotmail.com>
2026-02-07 18:17:05 -0300

    Alterando estados padroes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={` antialiased`}
       >
